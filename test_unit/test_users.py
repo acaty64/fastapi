b94cd12b3d029c9380b761b98b282ab5cd71eb56
PCUcss <acaty2@gmail.com>
2025-03-03 11:12:43 -0500

    User add by POST route with test_users.py

--- test_unit/test_users.py
+++ test_unit/test_users.py
@@ -9,7 +9,9 @@ def test_usersjson():
     response = client.get("/usersjson")
     assert response.status_code == 200
     assert type(response.json()) == list
+    print('len response: ', len(response.json()))
     assert len(response.json()) > 0
+
 
 def test_users():
     response = client.get("/users")
@@ -32,11 +34,19 @@ def test_user_error_by_id():
     assert jsonResponse['status'] == 'error'
 
 def test_add_user():
+    get_response = client.get("/users")
+    len_old = len(get_response.json())
+
     body = { "id":4, "name":"usuario_4", "age":20, "deactivate": False }
     response = client.post("/user", json=body)
     print(response.json()['message'])
     assert response.json()['message'] == 'Usuario agregado.'
     assert response.status_code == 200
+
+    get_response = client.get("/users")
+    len_new = len(get_response.json())
+    assert len_old + 1 == len_new
+
     response = client.post("/user", json=body)
     print(response.json()['message'])
     assert response.json()['message'] == 'El usuario ya existe.'
